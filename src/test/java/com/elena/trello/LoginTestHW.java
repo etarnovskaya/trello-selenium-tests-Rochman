1710b8cb7d2ad7ec341670b3af8a0be5af3a0461
etarnovskaya <etarnovskaya@gmail.com>
2019-12-25 19:44:48 +0200

    lesson 25_12 part1

--- src/test/java/com/elena/trello/LoginTestHW.java
+++ src/test/java/com/elena/trello/LoginTestHW.java
@@ -1,5 +1,6 @@
 package com.elena.trello;
 
+import org.openqa.selenium.By;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -8,10 +9,24 @@ public class LoginTestHW extends TestBase {
   @Test
   public void testLogInWithAtlassianAcc() throws InterruptedException {
     clickLoginLink();
-    fillLoginFormAtlassianAcc();
+    fillLoginFormAtlassianAcc("rochman.elena@gmail.com", "12345.com");
     pause(8000);
     Assert.assertTrue
             (isAvatarPresentOnHeader());
+  }
+
+  @Test
+  public void negativeTestLogInWithAtlassianAcc() throws InterruptedException {
+    clickLoginLink();
+    fillLoginFormAtlassianAcc("roch", "");
+    pause(8000);
+    Assert.assertTrue(isErrorPresent());
+    Assert.assertTrue
+            (!isAvatarPresentOnHeader());
+  }
+
+  public boolean isErrorPresent() {
+    return isElementPresent(By.id("error"));
   }
 
   @Test
--- src/test/java/com/elena/trello/TestBase.java
+++ src/test/java/com/elena/trello/TestBase.java
@@ -31,12 +31,12 @@ public class TestBase {
     Thread.sleep(millis);
   }
 
-  public void fillLoginFormAtlassianAcc() throws InterruptedException {
-    type(By.id("user"), "rochman.elena@gmail.com");
+  public void fillLoginFormAtlassianAcc(String user, String pwd) throws InterruptedException {
+    type(By.id("user"), user);
     pause(5000);
     click(By.id("login"));
     click(By.id("login-submit"));
-    type(By.id("password"), "12345.com");
+    type(By.id("password"), pwd);
     click(By.id("login-submit"));
   }
 
@@ -65,23 +65,23 @@ public class TestBase {
 
   public void loginWhithBothAccs() throws InterruptedException {
 
-//    type(By.id("user"), "rochman.elena@gmail.com");
-//    Thread.sleep(10000);
-//    if(wd.findElement(By.id("password")).isDisplayed()){
-//      type(By.id("password"), "12345.com");
-//    }
-//    click(By.id("login"));
-//
-//    if (isElementPresent(By.id("login-submit"))){
-//      click(By.id("login-submit"));
-//
-//      type(By.id("password"), "12345.com");
-//      click(By.id("login-submit"));
-//    }
+    type(By.id("user"), "rochman.elena@gmail.com");
+    Thread.sleep(10000);
+    if(wd.findElement(By.id("password")).isDisplayed()){
+      type(By.id("password"), "12345.com");
+    }
+    click(By.id("login"));
+
+    if (isElementPresent(By.id("login-submit"))){
+      click(By.id("login-submit"));
+
+      type(By.id("password"), "12345.com");
+      click(By.id("login-submit"));
+    }
   }
 
   public boolean isAvatarPresentOnHeader() {
     return isElementPresent
-            (By.cssSelector("[data-test-id='header-member-menu-button']"));
+            (By.cssSelector("[djata-test-id='header-member-menu-button']"));
   }
 }
