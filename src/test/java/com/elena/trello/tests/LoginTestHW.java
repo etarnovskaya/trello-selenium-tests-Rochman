ec66df1ec6a3f0829e14ae2b50411070798277d0
etarnovskaya <etarnovskaya@gmail.com>
2020-02-05 19:07:20 +0200

    commit

--- src/test/java/com/elena/trello/tests/LoginTestHW.java
+++ src/test/java/com/elena/trello/tests/LoginTestHW.java
@@ -6,7 +6,7 @@ import org.testng.annotations.Test;
 
 public class LoginTestHW extends TestBase {
 @BeforeMethod
-public void ensurePreconditions(){
+public void ensurePreconditions() throws InterruptedException {
   if(app.getSession().isAvatarPresentOnHeader()){
     app.getSession().logout();
 
